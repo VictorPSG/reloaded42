204f91b5200af3ff879e8bed6980f6a46949825e
Victor Paulo Da Silva Garcia <victda-s@c2r6p6.42sp.org.br>
2024-10-11 17:38:26 -0300

    Conclusao ex27

--- ex27/ft_display_file.c
+++ ex27/ft_display_file.c
@@ -6,16 +6,18 @@
 /*   By: victda-s <victda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 21:20:54 by victda-s          #+#    #+#             */
-/*   Updated: 2024/10/11 16:56:24 by victda-s         ###   ########.fr       */
+/*   Updated: 2024/10/11 17:37:34 by victda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>  // Para open()
-#include <unistd.h> // Para read(), write()
+#include <fcntl.h>
+#include <unistd.h>
 
 int	open_file(int num_argc, char *file)
 {
-	int fd; // Usar `int` para o descritor de arquivo
+	int	fd;
+
+	fd = 0;
 	if (num_argc == 2)
 	{
 		fd = open(file, O_RDONLY);
@@ -25,29 +27,26 @@ int	open_file(int num_argc, char *file)
 
 int	main(int argc, char *argv[])
 {
-	char c;
-	int fd;
+	int		fd;
+	char	c;
 
-	if (argc < 2) 
+	if (argc < 2)
 	{
-		write(1, "File name missing.", 19);
+		write(1, "File name missing.\n", 20);
 		return (1);
 	}
-	if(argc > 2)
+	if (argc > 2)
 	{
-		write(1, "Too many arguments.", 20);
+		write(1, "Too many arguments.\n", 21);
+		return (0);
 	}
-
 	fd = open_file(argc, argv[1]);
-
 	if (fd == -1)
-		return (1); // Saída em caso de erro ao abrir arquivo
-
-	while (read(fd, &c, 1) > 0) // Lê o arquivo caractere por caractere
+		return (1);
+	while (read(fd, &c, 1) > 0)
 	{
-		write(1, &c, 1); // Imprime caractere lido
+		write(1, &c, 1);
 	}
-
 	close(fd);
 	return (0);
 }
