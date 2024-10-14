5430624bf963bb9e486b94c86350268613c2e19f
Victor Paulo Da Silva Garcia <victda-s@c1r10p4.42sp.org.br>
2024-10-13 21:23:57 -0300

    correcao ex21

--- ex21/ft_range.c
+++ ex21/ft_range.c
@@ -6,11 +6,12 @@
 /*   By: victda-s <victda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 17:39:17 by victda-s          #+#    #+#             */
-/*   Updated: 2024/10/09 10:23:16 by victda-s         ###   ########.fr       */
+/*   Updated: 2024/10/13 21:23:25 by victda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 
 int	*ft_range(int min, int max)
 {
@@ -18,28 +19,57 @@ int	*ft_range(int min, int max)
 	int	*arr;
 	int	i;
 
+	if (min >= max)
+	{
+		return (0);
+	}
 	diff = max - min;
 	arr = (int *) malloc(diff * sizeof(int));
 	i = 0;
-	while (i <= diff)
+	if (arr == NULL)
+		return (0);
+	while (i < diff)
 	{
 		arr[i] = min;
 		min++;
 		i++;
 	}
-	arr [min] = '\0';
 	return (arr);
 }
 /*
 int main(void)
 {
- int *array;
+	int i;
+	int min = 0, max = 10;
+	int *array = ft_range(min, max);
 
- array = ft_range(-3, 10);
- for (int i = 0; array[i] != '\0'; i++)
- { 
-  printf("aqui: %d \n", array[i]);
- }
- free(array);
+	i = 0;
+	if(array != NULL)
+	{
+		while(i < (max - min))
+		{
+			 printf("%d\n", array[i]);
+			 i++;
+		}
+	}
 }
+
+
+
+
+// int main(void)
+// {
+//  int *array;
+
+//  array = ft_range(10, 0);
+//  if(array == 0)
+//  {
+// 	return (0);
+//  }
+//  for (int i = 0; array[i] != '\0'; i++)
+//  { 
+//   printf("aqui: %d \n", array[i]);
+//  }
+//  free(array);
+// }
 */
