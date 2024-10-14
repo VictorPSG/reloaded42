edc2e4f317c2095df8717f1da29fa27d00493b9e
Victor Paulo Da Silva Garcia <victda-s@c1r10p4.42sp.org.br>
2024-10-13 21:17:03 -0300

    conclusao ex17

--- ex17/ft_strcmp.c
+++ ex17/ft_strcmp.c
@@ -6,33 +6,28 @@
 /*   By: victda-s <victda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 20:53:08 by victda-s          #+#    #+#             */
-/*   Updated: 2024/10/06 20:55:03 by victda-s         ###   ########.fr       */
+/*   Updated: 2024/10/13 21:16:25 by victda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
-	int	lenght;
 
-	lenght = 0;
 	i = 0;
-	while (s1[lenght] != '\0')
-		lenght++;
-	while (i <= lenght)
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			if (s1[i] > s2[i])
-				return (1);
-			else
-				return (-1);
-			i++;
+			return (s1[i] - s2[i]);
 		}
+		i++;
 	}
 	return (0);
 }
 /*
+#include <stdio.h>
+#include <string.h>
 int main(void)
 {
  char teste[] = "apple";
@@ -41,4 +36,5 @@ int main(void)
  printf("Função FT: %d\n", ft_strcmp(teste, teste1));
  printf("Função strcmp: %d", strcmp(teste, teste1));
 }
+
 */
